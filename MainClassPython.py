4816a047ef0407b78eb6b8197cad808e9a97db96
SAMUEL MONTIEL SANTOS <smontieln@gmail.com>
2023-01-12 16:36:49 -0600

    Se agregan flujos restantes y bibleotecas para calculos

--- MainClassPython.py
+++ MainClassPython.py
@@ -1,5 +1,6 @@
 ##Bibleotecas
-
+import math
+import numpy as np
 
 ##Variables
 action = 0;
@@ -125,6 +126,127 @@ while action != 10:
             except:
                 print("Sólo valores numéricos")
             print("\nEl resultado fue: ", numeroUno / numeroDos)
+
+            respuesta = 0
+
+            try:
+                respuesta = int(input("¿Deseas realizar otra operación Sí(1) / No(Cualquier caracter)?: "))
+            except:
+                print("Sólo números como respuestas")
+
+            if respuesta != 1:
+                salirFlujoSecundario = True
+
+
+    ## ******Raíz cuadrada********
+    if action == 5:
+        salirFlujoSecundario = False
+        while salirFlujoSecundario != True:
+
+            print("Elegiste Raíz Cuadrada \n*************")
+
+            try:
+                numeroUno = float(input("Introduce un número para obtener su raíz: "))
+            except:
+                print("Sólo valores numéricos")
+            print("\nEl resultado fue: ", math.sqrt(numeroUno))
+
+            respuesta = 0
+
+            try:
+                respuesta = int(input("¿Deseas realizar otra operación Sí(1) / No(Cualquier caracter)?: "))
+            except:
+                print("Sólo números como respuestas")
+
+            if respuesta != 1:
+                salirFlujoSecundario = True
+
+    ## ******Exponente********
+    if action == 6:
+        salirFlujoSecundario = False
+        while salirFlujoSecundario != True:
+
+            print("Elegiste Exponente \n*************")
+
+            try:
+                numeroUno = float(input("Introduce el número BASE: "))
+            except:
+                print("Sólo valores numéricos")
+            try:
+                numeroDos = float(input("Introduce el EXPONENTE: "))
+            except:
+                print("Sólo valores numéricos")
+            print("\nEl resultado fue: ", pow(numeroUno,numeroDos))
+
+            respuesta = 0
+
+            try:
+                respuesta = int(input("¿Deseas realizar otra operación Sí(1) / No(Cualquier caracter)?: "))
+            except:
+                print("Sólo números como respuestas")
+
+            if respuesta != 1:
+                salirFlujoSecundario = True
+
+
+    ## ******Seno********
+    if action == 7:
+        salirFlujoSecundario = False
+        while salirFlujoSecundario != True:
+
+            print("Elegiste Seno \n*************")
+
+            try:
+                numeroUno = float(input("Introduce un número para calcula el seno: "))
+            except:
+                print("Sólo valores numéricos")
+            print("\nEl resultado fue: ", np.sin(np.radians(numeroUno)))
+
+            respuesta = 0
+
+            try:
+                respuesta = int(input("¿Deseas realizar otra operación Sí(1) / No(Cualquier caracter)?: "))
+            except:
+                print("Sólo números como respuestas")
+
+            if respuesta != 1:
+                salirFlujoSecundario = True
+
+    ## ******Coseno********
+    if action == 8:
+        salirFlujoSecundario = False
+        while salirFlujoSecundario != True:
+
+            print("Elegiste Coseno \n*************")
+
+            try:
+                numeroUno = float(input("Introduce un número para calcula el coseno: "))
+            except:
+                print("Sólo valores numéricos")
+            print("\nEl resultado fue: ", np.cos(np.radians(numeroUno)))
+
+            respuesta = 0
+
+            try:
+                respuesta = int(input("¿Deseas realizar otra operación Sí(1) / No(Cualquier caracter)?: "))
+            except:
+                print("Sólo números como respuestas")
+
+            if respuesta != 1:
+                salirFlujoSecundario = True
+
+    ## ******Tangente********
+    if action == 9:
+        salirFlujoSecundario = False
+        while salirFlujoSecundario != True:
+
+            print("Elegiste Tangente \n*************")
+
+            try:
+                numeroUno = float(input("Introduce un número para calcula la tangente: "))
+            except:
+                print("Sólo valores numéricos")
+            print("\nEl resultado fue: ", np.tan(np.radians(numeroUno)))
 
             respuesta = 0
 
